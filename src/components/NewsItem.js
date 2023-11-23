44bbb845e1ce7af75742783b258e8cb0674d3259
kkumardeshmukh <kdkrantikumar@gmail.com>
2023-11-23 17:00:06 +0530

    basic structure of app is ready

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class NewsItem extends Component {
+
+  render() {
+    return (
+      <div>
+        This is newsitem which you find.
+      </div>
+    )
+  }
+}
