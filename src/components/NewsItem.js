c80c077e30f14e8b113f08c4390d2b107363d27e
kkumardeshmukh <kdkrantikumar@gmail.com>
2023-11-24 13:18:44 +0530

    card with actual fetched news is ready to populate

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,11 +11,11 @@ export default class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="Image not found"/>
+          <img src={imageUrl} className="card-img-top" alt="N/A"/>
             <div className="card-body">
               <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}...</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+              <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
       </div>
